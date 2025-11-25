5448bb3e3c4e48056fa52fcf10a57741c09b5dba
Arjun Sikarwar <arjunsikarwar@Arjuns-MacBook-Air.local>
2025-11-25 11:51:11 +0530

    Creating a Complaint Controller

---
+++ Backend/src/main/java/SecurityManagementBackend/Backend/Controller/ComplaintController/ComplaintControllerImplementation.java
@@ -0,0 +1,18 @@
+package SecurityManagementBackend.Backend.Controller.ComplaintController;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/complaint")
+public class ComplaintControllerImplementation {
+
+
+    @PostMapping("/createNewComplaint")
+    public void createNewComplaint(){
+
+    }
+
+
+}
