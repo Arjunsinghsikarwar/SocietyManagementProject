abf85e9402d417480b1adb4a26721bc4e2d501ba
Arjun Sikarwar <arjunsikarwar@Arjuns-MacBook-Air.local>
2025-11-25 12:07:09 +0530

    Creating one more method

--- Backend/src/main/java/SecurityManagementBackend/Backend/Controller/ComplaintController/ComplaintControllerImplementation.java
+++ Backend/src/main/java/SecurityManagementBackend/Backend/Controller/ComplaintController/ComplaintControllerImplementation.java
@@ -1,5 +1,6 @@
 package SecurityManagementBackend.Backend.Controller.ComplaintController;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class ComplaintControllerImplementation {
 
     @PostMapping("/createNewComplaint")
     public void createNewComplaint(){
+
+    }
+
+    @GetMapping("getAllComplaints")
+    public void getAllComplaints(){
 
     }
 
